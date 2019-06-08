af6ff0e55d3957d7ce05040836361bc1f4675da0
brenolazari <brenolazari1983@gmail.com>
2019-06-08 13:10:26 -0300

    Added threading; still needs testing

--- listener.py
+++ listener.py
@@ -1,8 +1,11 @@
 import socket, sys
 from struct import *
+from _thread import *
+import threading
 
 PROTOCOL_TYPE_IPV6 = 0x86dd
 
+## This is for the TCP flags statements
 # 000010
 FLAGS_SYN = 2
 # 010010 
@@ -14,17 +17,50 @@ FLAGS_RST = 4
 # 010000
 FLAGS_ACK = 16
 
-def listener():
-    listen = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
-    print("Starting Listener")
+def threaded(listen):
+    print("Started thread...")
     while True:
+        # We should put this packet receiving code inside a single function for reusability
+        # Receive packet
         raw_packet = listen.recvfrom(65565)
         packet = raw_packet[0]
-        # Now we need to unpack the packet. It will be an TCP packet
-        # We want to pull out and compare only these three
 
-        # This is the TCP header. Normal length is 20 bytes.
+        # Get ethernet header
+        eth_header = packet[0:14]
 
+        # Get protocol type; 0x86dd for IPv6
+        protocol_type = unpack('!6B6BH', eth_header)[12]
+
+        # Check for IPv6 only
+        if (protocol_type == int(PROTOCOL_TYPE_IPV6)):
+            print("Received IPv6 packet inside thread.")
+
+            # Get TCP header
+            tcp_header = unpack('!HHLLBBHHH', packet[54:74])
+
+            # Get TCP flags
+            flags = int(tcp_header[5])
+
+            if(flags == FLAGS_ACK):
+                print("!! RECEIVED A TCP CONNECT ATTACK !!")
+                break
+            elif(flags == FLAGS_RST):
+                print("!! RECEIVED A TCP HALF OPENING ATTACK !!")
+                break
+
+    print("Exiting thread...")
+
+def listener():
+    # Creates raw socket
+    listen = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
+
+    print("Starting Listener...")
+    while True:
+        # We should put this packet receiving code inside a single function for reusability
+        # Receive packet
+        raw_packet = listen.recvfrom(65565)
+        packet = raw_packet[0]
+        
         # Get ethernet header
         eth_header = packet[0:14]
 
@@ -33,30 +69,23 @@ def listener():
     
         # Check for IPv6 only
         if (protocol_type == int(PROTOCOL_TYPE_IPV6)):
+
+            # Get TCP header
             tcp_header = unpack('!HHLLBBHHH', packet[54:74])
+
+            # Get TCP flags
             flags = int(tcp_header[5])
             print(flags)
 
-            urg = tcp_header[5] >> 5 & 1
-            ack = tcp_header[5] >> 4 & 1
-            psh = tcp_header[5] >> 3 & 1
-            rst = tcp_header[5] >> 2 & 1
-            syn = tcp_header[5] >> 1 & 1
-            fin = tcp_header[5] >> 0 & 1
-
             # This listener should warn which type of attack it is receiving
-    
             # TCP connect attack
             # expected: SYN and later an ACK
             # TCP half-opening
             # expected: SYN and later an RST
 
             if (flags == FLAGS_SYN):
-                print('esperar pacote com ack(tcp connect) ou rst(tcp half-opening)')
-                print('aha2: ', bin(flags))
                 # await ACK or RST
-                # thread
-                # fork
+                start_new_thread(threaded, (listen,))
 
             # Stealth scan ou TCP FIN
             # expected: FIN
