68e5c04e9019fd8c8222d5651aca089a8fde007b
BernardoKoefender <bernardokoefender@hotmail.com>
2019-06-12 04:21:28 -0300

    dudu

--- listener.py
+++ listener.py
@@ -27,6 +27,7 @@ WND_TITLE      = "ListenerV6"
 
 SHUTDOWN = False
 
+PACKET_QUEUE = []
 IPV6_LIST_IN = []
 
 # Packet example:
@@ -49,143 +50,199 @@ IPV6_LIST_IN = []
 # 70 8d18                                  CHEKSUM
 # 72 0000                                  URGENT POINTER
 
-# Stores IPV6 TCP packets
+
 class packetStorage:
-    # Initializer
-    def __init__(self, packet, time):
-        self.packet      = packet
-        self.timestamp   = time
+    def __init__(self, packet, time, display):
+        """
+        Stores several information about an ip packet
+        """
+        self.packet = packet
+        self.timestamp = time
         self.avginterval = 0
-        self.smac        = self.packet[ 0: 6].hex()
-        self.dmac        = self.packet[ 6:12].hex()
-        self.sip         = self.packet[22:38].hex()
-        self.dip         = self.packet[38:54].hex()
+        self.smac = self.packet[ 0: 6].hex()
+        self.dmac = self.packet[ 6:12].hex()
+        self.sip  = self.packet[22:38].hex()
+        self.dip  = self.packet[38:54].hex()
 
         self.sport = []
         self.sport.append(self.packet[54:56].hex())
         self.dport = []
         self.dport.append(self.packet[56:58].hex())
 
+        self.frm_line = tk.Frame(display, width = 539, height = 20, bg = 'white smoke')
+        self.lbl_ip = tk.Label(self.frm_line, width = 22, text = "fe80::0000:0000:0000:a61f", bg = 'white smoke')
+        self.var_dport_len = tk.IntVar()
+        self.var_dport_len.set(len(self.dport))
+        self.lbl_dport_len = tk.Label(self.frm_line, width = 5, textvariable = self.var_dport_len, bg = 'gainsboro')
+        
+        # Labels and variables that represent the number of times that 'X' flag was used by 'Y' IPv6.
+        self.var_syn_len = tk.IntVar()
+        self.var_syn_len.set(0)
+        self.lbl_syn_count = tk.Label(self.frm_line, width = 5, textvariable = self.var_syn_len, bg = 'white smoke')
+        self.var_ack_len = tk.IntVar()
+        self.var_ack_len.set(0)
+        self.lbl_ack_count = tk.Label(self.frm_line, width = 5, textvariable = self.var_ack_len, bg = 'gainsboro')
+        self.var_rst_len = tk.IntVar()
+        self.var_rst_len.set(0)
+        self.lbl_rst_count = tk.Label(self.frm_line, width = 5, textvariable = self.var_rst_len, bg = 'white smoke')
+        self.var_fin_len = tk.IntVar()
+        self.var_fin_len.set(0)
+        self.lbl_fin_count = tk.Label(self.frm_line, width = 5, textvariable = self.var_fin_len, bg = 'gainsboro')
+
+        self.frm_line.place(x = 0, y = 0)
+        self.lbl_ip.place(x = 0, y = 0)
+        self.lbl_dport_len.place(x = 182, y = 0)
+        self.lbl_syn_count.place(x = 225, y  = 0)
+        self.lbl_ack_count.place(x = 272, y  = 0)
+        self.lbl_rst_count.place(x = 319, y  = 0)
+        self.lbl_fin_count.place(x = 364, y  = 0)
+
+        self.updateFlags(packet[66:68].hex())
+
+    def addPort(self, port):
+        self.dport.append(port)
+        self.var_dport_len.set(len(self.dport))
+
+
+    def updateFlags(self, flags):
+        print(flags)
+        if int(flags)&FLAGS_SYN:
+            self.var_syn_len.set(self.var_syn_len.get()+1)
+        if int(flags)&FLAGS_ACK:
+            self.var_ack_len.set(self.var_ack_len.get()+1)
+        if int(flags)&FLAGS_RST:
+            self.var_rst_len.set(self.var_rst_len.get()+1)
+        if int(flags)&FLAGS_FIN:
+            self.var_fin_len.set(self.var_fin_len.get()+1)
+
+
+
     # Checks if IP is already stored, returns true if it is
     def checkIfIpStored(self, packet):
         global IPV6_LIST_IN
+        # Check if ipv6 list is empty
         if IPV6_LIST_IN == []:
             return False
         elif self.sip == packet[22:38].hex():
             return True
         return False
 
-    # Checks if port was already used
+
+    # Checks if dport was already used by given ip packet
     def checkIfPortStored(self, packet):
         global IPV6_LIST_IN
         for p in self.dport:
-            print(p,packet[56:58].hex())
             if p == packet[56:58].hex():
                 return True
         return False
 
-        
 
-#####################
 class listenerWindow:
     def __init__(self, master):
+        """
+        Defines the window and main functionalities of the software
+        """
         self.master = master
+        # Flag for incoming packet
+        self.var_new_packet = tk.BooleanVar()
+        self.var_new_packet.set(False)
+        self.var_new_packet.trace('w', self.handlePacketQueue)
+        # Basic window configuration
         self.master.title(WND_TITLE)
         self.master.geometry(WND_RESOLUTION)
-        self.listernerThread = threading.Thread(target = self.listener)
         self.w, self.h = WND_RESOLUTION.split("x",1)
         self.w, self.h = int(self.w), int(self.h)
-
-        self.lblPacketList = []
-
+        # Thread that represents the listener
+        self.listernerThread = threading.Thread(target = self.listener)
+        # Main frame, parent of all widgets
         self.frmMain = tk.Frame(master, width = self.w, height = self.h, bg = 'white')
-        self.frmDisplay = tk.Frame(self.frmMain, width = (self.w-260), height = (self.h-60), bg = 'white smoke', highlightthickness = 1, highlightbackground = 'grey')
-
+        # Meaning of each field on the window connections display
+        subtitles  = "Port                   \n"
+        subtitles += "                    IPv6                    range   #SYN   #ACK   #RST   #FIN                     "
+        self.lbl_subtitles = tk.Label(self.frmMain, text = subtitles, bg = 'white', font = ('helvetica', 10) )
+        # Display frame, where informations about connections are displayed
+        self.frm_display = tk.Frame(self.frmMain, width = (self.w-260), height = (self.h-60), bg = 'white smoke', highlightthickness = 2, highlightbackground = 'silver')
+        # Button to exit the software
         self.btnCloseApp = tk.Button(self.frmMain, text="Exit", command = self.quitListener, height = 2, width = 6, bg = 'white', activebackground = 'white smoke' )
-
+        # Placement of widgets
         self.frmMain.place(x = 0, y = 0)
-        self.frmDisplay.place( x = 250, y = 50 )
+        self.lbl_subtitles.place( x = 250, y = 10)
+        self.frm_display.place( x = 250, y = 50 )
         self.btnCloseApp.place(x = 10, y = (self.h-50))
 
 
-    def updateLblList(self):
-        # If empty list
-        #if self.lblPacketList == []:
-            #self.lblPacketList.append( tk.Label( self.frmDisplay, text = "24323", width = (67) ))
-            #self.lblPacketList[0].place(x = 0, y = 0)
-
-        # Check for all stored packets
-        if len(IPV6_LIST_IN) != len(self.lblPacketList):
-
-            for p in IPV6_LIST_IN:
-
-                if p.sip() == 
-
-            # Update labels
-
-            
-
-
-            #for lbl in self.lblPacketList:
-
-                #if self.lblPacketList[0].cget("text") == 
-                #print( self.lblPacketList[0].cget("text") )
-
-
+    def updateLabelGrid(self):
+        aux = 0;
+        for pckt in IPV6_LIST_IN:
+            #pckt.frm_line.pack_forget()
+            #pckt.frm_line.place(x = 0, y = (0 + 5*aux))
+            aux += 1
 
 
     def quitListener(self):
+        """
+        Raise SHUTDOWN flag, close listener thread and shut down tkinter window.
+        """
+
         global SHUTDOWN
         SHUTDOWN = 1
         self.listernerThread.join()
         self.master.quit()
 
 
+    def handlePacketQueue(self, *args):
+        if self.var_new_packet.get():
+            while len(PACKET_QUEUE) > 0:
+                packet = PACKET_QUEUE.pop()
+                if IPV6_LIST_IN == []:
+                    IPV6_LIST_IN.append(packetStorage(packet, datetime.now(), self.frm_display))
+                else:
+                    for p in IPV6_LIST_IN:
+                        if not p.checkIfIpStored(packet):
+                            IPV6_LIST_IN.append(packetStorage(packet, datetime.now(), self.frm_display))
+                        elif not p.checkIfPortStored(packet):
+                            p.addPort((packet[56:58].hex()))
+                            p.updateFlags(packet[66:68].hex())
+                            break
+                        else:
+                            p.updateFlags(packet[66:68].hex())
+
+            self.var_new_packet.set(False)
+
+
     def listener(self):
         """
         This listener represents the main attack detection component.
         """
-
         global SHUTDOWN
         global IPV6_LIST_IN
-
         # Creates raw socket
         listen = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
 
-        print("Starting Listener...")
+        # Main loop
         while not SHUTDOWN:
             # We should put this packet receiving code inside a single function for reusability
+
             # Receive packet
             raw_packet = listen.recvfrom(65565)
             packet = raw_packet[0]
+
             # Get ethernet header
             eth_header = packet[0:14]
 
             # Get protocol type; 0x86dd for IPv6
             protocol_type = unpack('!6B6BH', eth_header)[12]
 
-            #Next header
             next_header = (packet.hex()[40:42])
-
+            #self.var_new_packet.set(False)
             # Check if TCP IPv6
             if (protocol_type == int(PROTOCOL_TYPE_IPV6) and next_header == "06"):
 
-                # Add packet to list
-                # Check for port
-                if IPV6_LIST_IN == []:
-                    IPV6_LIST_IN.append( packetStorage(packet, datetime.now()))
-                else:
-                    for p in IPV6_LIST_IN:
-                        if not p.checkIfIpStored(packet):
-                            IPV6_LIST_IN.append( packetStorage(packet, datetime.now()))
-                        if not p.checkIfPortStored(packet):
-                            p.dport.append(packet[56:58].hex())
-                            print(p.sip, " -> ", p.dport)
-                            break
+                # Add packet to queue
+                PACKET_QUEUE.append(packet)
+                self.var_new_packet.set(True)
 
-                self.updateLblList()
-
-                #print(IPV6_LIST_IN)
+                #print(self.var_new_packet.get())
 
                 # Get TCP header
                 tcp_header = unpack('!HHLLBBHHH', packet[54:74])
@@ -284,7 +341,7 @@ def get_source_mac(eth_header):
 
     return source_mac
 
-def listener(wnd):
+def lisssstener(wnd):
     """
     This listener represents the main attack detection component.
     """
@@ -362,7 +419,6 @@ def listener(wnd):
             # SYN/ACK attack handling
             #elif (flags == FLAGS_SYN_ACK):
                 #print("!! RECEIVED SYN/ACK ATTACK FROM MAC ADDRESS {} on port {} !!".format(source_mac, target_port))
-    
         
 
 if __name__ == "__main__":
